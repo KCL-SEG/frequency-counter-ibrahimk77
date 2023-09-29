04e228c593ff8e8a435ec89bc6af380515797c15
Ibrahim <k22013981@nmslab-005828.nms.kcl.ac.uk>
2023-09-29 14:28:17 +0100

    Solution 3

--- frequencies.py
+++ frequencies.py
@@ -4,7 +4,7 @@
 def frequencies(items):
     frequencies = {}
 
-    for item in items:
+    for str(item) in items:
         if item in frequencies.keys():
             frequencies[str(item)] += 1
         else:
