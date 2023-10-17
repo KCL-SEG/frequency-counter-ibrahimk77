56c5c0a6bf95364bdd84575aea196809ad9382e3
Ibrahim <04ibrahimkhan04@gmail.com>
2023-10-18 00:27:51 +0100

    test

--- frequencies.py
+++ frequencies.py
@@ -8,7 +8,7 @@ def frequencies(items):
         if str(item) in frequencies.keys():
             frequencies[str(item)] += 1
         else:
-            frequencies[str(item)] = 1
+            frequencies[str(item)] = 1 
     
 
     return frequencies
